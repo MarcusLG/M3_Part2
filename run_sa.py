278d3cf0630a64da2d5c0b1faf9ba0426cbe0e20
Marcus Lim <marcuslim@Marcuss-MacBook-Air.local>
2023-01-02 10:06:18 +0000

    Adding SA script

---
+++ run_sa.py
@@ -0,0 +1,6 @@
+import part2
+
+overwrite_file = False
+num_initialization = 1200
+part2.simulated_annealing(num_initialization, 1000, 5, 4000, overwrite_file)
+# def simulated_annealing(n_iterations, step_size, temp, overwrite=False):
